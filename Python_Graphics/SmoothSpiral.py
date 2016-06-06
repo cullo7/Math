ac75facec1ddad4231541dc927ee3ec74d09b040
cullo7 <culloaiden3@gmail.com>
2016-06-06 04:33:37 -0400

    reduced width, made curve smoother

--- Python_Graphics/SmoothSpiral.py
+++ Python_Graphics/SmoothSpiral.py
@@ -2,29 +2,36 @@ import pygame
 import math
 import sys
 
-def drawSpiral(screen, count, diameterIncrement):
+### SMOOTH SPIRAL ###
+
+def drawSmoothSpiral(screen, count):
+  #drawing arcs already covered in a for loop
   for x in range(0,count):
     countdown = count - x
-    angStart = (math.pi * (countdown))/2
-    diameter = 10+(countdown)*diameterIncrement
-    pygame.draw.arc(screen, (255,255,255), pygame.Rect(600-countdown*diameterIncrement/2, 350-countdown*diameterIncrement/2, diameter, diameter), angStart, angStart+math.pi/2, 1)
+    angStart = (math.pi * (countdown))/10
+    diameter = 2*countdown
+    pygame.draw.arc(screen, (255,255,255), pygame.Rect(600-countdown, 350-countdown, diameter, diameter), angStart, angStart+math.pi/2, 1)
+
 def main():
+  #Set Window dimensions
   windowWidth = 1200
   windowHeight = 700
   pygame.init()
   screen = pygame.display.set_mode((windowWidth, windowHeight))
   screen.fill((0,0,0)) 
   i=0
+  
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()  
     pygame.display.update()
-    if i < 20:
-      drawSpiral(screen, i, 20)
+    if i < 200:
+      drawSmoothSpiral(screen, i)
       i+=1
-      pygame.time.wait(500)
+      pygame.time.wait(1)
     pygame.display.update()
+
 if __name__ == "__main__":
   main()
